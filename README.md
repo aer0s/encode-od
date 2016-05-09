9c1dfaa0d4558ea156c82dbb40614910ad616865
Jason Noll <jinoll@avmats.com>
2016-05-09 15:45:14 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # encode-od
-Auto encode optical disc with makemkv-cli, hanbrake-cli with subtitles
+Auto encode optical disc with makemkv-cli, hanbrake-cli, ffmpeg with subtitles. This script will create two encoded files: __mkv__ & __m4v__
+
+## Prerequisites
+ - python >= 2.7
+ - makemkvcon
+ - HandBrakeCLI
+ - vobsub2srt
+ - mkvtoolnix
+
+## Usage Quick Guide
+Insert a DVD or BD and then execute the following command:
+```bash
+$ ./local/bin/encode-od #will encode the content of /dev/sr0
+
+# encode /dev/sr1 to a folder in the /tmp directory.
+$ ./local/bin/encode-od -s 1 -o /tmp/movies
+
+```
+
+### Options
+ - __--source, -s__ : Specify the Device Source of the optical media. ie __0__ for */dev/sr__0__*
+ - __--output, -o__ : Specify the absoulte path Output Directory for the encode. By default it will use a folder called __*output*__ in the encode_od git directory.
---
+++ local/lib/python/encode_od/cli.py
@@ -0,0 +1,142 @@
+from subprocess import call, check_output, PIPE, Popen
+import os, sys, json
+import click
+if float(sys.version[:3]) < 3.3:
+    from pipes import quote
+else:
+    from shlex import quote
+
+base_path = os.path.dirname(os.path.abspath(__file__))
+
+def get_dvd_name(source):
+    cmd = [' '.join([
+        "vobcopy", "-i", "/dev/sr%d" % source, "-I", "2>&1", ">", "/dev/stdout",
+        "|", "grep","DVD-name","|",
+        "sed","-e","'s/.*DVD-name: //'",
+        ])]
+    r = Popen(cmd, shell=True, stdout=PIPE)
+    (output, err) = r.communicate()
+    result = output.decode('utf-8').strip()
+    return result.replace('_', ' ').title()
+
+def ffmpeg(source,title):
+    opts = {
+        'src': quote(os.path.join(source,'%s.mkv' % title)),
+        'dest': quote(os.path.join(source, '%s.m4v' % title)),
+        'srt': quote(os.path.join(source, '%s.srt' % title)),
+        }
+    result = shell([' '.join([
+            'ffmpeg','-i', opts['src'], '-i', opts['srt'],
+            '-map', '0:v', '-map', '0:a', '-map', '1:s',
+            '-c:v', 'copy', '-vcodec', 'libx264',
+            '-c:a', 'copy', '-acodec', 'aac',
+            '-c:s', 'mov_text', opts['dest']])
+        ], shell=True)
+    return result
+
+def handbrake(source,title):
+    opts = {
+        'src': quote(os.path.join(source,'%s.mkv' % title)),
+        'dest': quote(os.path.join(source, '%s.mp4' % title)),
+        }
+    result = shell(
+        ['HandBrakeCLI -i %(src)s -o %(dest)s --preset="AppleTV 3"' % opts],
+        shell=True)
+    return result
+
+def makemkv(source,output,title):
+    print('Running makemkv')
+    shell('makemkvcon mkv --decrypt --cache=16 -r disc:%d all %s' % (source,
+        output), stream=True)
+
+    # Find which files to keep
+    fl = os.listdir(output)
+    keep = -1
+    destroy = [os.path.join(output, f) for f in fl]
+    sizes = [os.path.getsize(f) for f in destroy]
+    biggest = sizes.index(max(sizes))
+    del destroy[biggest]
+
+    print('Cleaning up')
+    [os.remove(os.path.join(output, f)) for f in destroy]
+    save_path = os.path.join(output, '%s.mkv' % title)
+    os.rename(os.path.join(output, fl[biggest]), save_path)
+
+@click.command()
+@click.option('--source', '-s', default=0,
+    help='Device Source Integer, ie /dev/sr0 [default: 0]')
+@click.option('--output', '-o', default=os.path.join(base_path, 'output'),
+    help='Absolute path of Output Directory [default: ./output]')
+def main(source, output):
+    # get the movie title
+    title = get_dvd_name(source)
+    # title = 'Daddys Home'
+    # make necessary directories
+    groupP = os.path.join(output, title)
+    os.makedirs(output, exist_ok=True)
+    os.makedirs(groupP, exist_ok=True)
+
+    # rip to mkv
+    makemkv(source, groupP, title)
+
+    # Convert and insert subtitles
+    convert_subtitles(groupP, title)
+
+    # Encode mp4
+    ffmpeg(groupP, title)
+
+
+def convert_subtitles(output, title):
+    sfiles = {
+        'mkv': quote(os.path.join(output, '%s.mkv' % title)),
+        'mkvCopy': quote(os.path.join(output, '%s-working.mkv' % title)),
+        'idx': quote(os.path.join(output, '%s.idx' % title)),
+        'srt': quote(os.path.join(output, '%s.srt' % title)),
+        'sub': quote(os.path.join(output, '%s' % title)),
+    }
+    # get and index of mkv tracks
+    r = shell(['mkvmerge -i -F json %(mkv)s' % sfiles], shell=True)
+    subs = []
+    for track in json.loads(r).get('tracks',[]):
+        # ident english subtitle files and extract
+        if track.get('type', '') in 'subtitles':
+            subs.append(track.get('id'))
+            if track.get('properties',{}).get('language','') in 'eng':
+                sfiles['id'] = track.get('id',False)
+
+    if sfiles['id']:
+        print('Extracting subtitles... be patient this is not fast')
+        shell([
+            'mkvextract tracks %(mkv)s %(id)d:%(idx)s' % sfiles,
+        ],shell=True)
+        print('Converting optical subtitles to text (OCR)...')
+        shell(['vobsub2srt %(sub)s' % sfiles,], shell=True)
+        print('Subtitles converted, begining merge...')
+        shell([
+            "mkvmerge -o %(mkvCopy)s --subtitle-tracks %(id)d %(mkv)s %(srt)s" % sfiles,
+            ], shell=True)
+        print('Cleaning up ...')
+        os.remove(os.path.join(output, '%s.idx' % title))
+        os.remove(os.path.join(output, '%s.sub' % title))
+        os.remove(os.path.join(output, '%s.mkv' % title))
+        os.rename(os.path.join(output, '%s-working.mkv' % title), os.path.join(output, '%s.mkv' % title))
+        print('done.')
+    else:
+        print('Well damn it!')
+
+
+def shell(cmd, **kwargs):
+    if type(cmd) == str:
+        cmd = cmd.split(' ')
+
+    if kwargs.get('stream',False):
+        call(cmd)
+    elif kwargs.get('shell', False):
+        r = Popen(cmd, stdout=PIPE, shell=True)
+        (output, err) = r.communicate()
+        return output.decode('utf-8')
+    else:
+        return check_output(cmd)
+
+if __name__ == "__main__":
+    main()
