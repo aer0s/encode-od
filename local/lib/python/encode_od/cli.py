db7ca305eed0afcb86b96a69a636a31189e7c97f
Jason <jason@naAce.nollhome>
2016-05-20 21:20:44 -0500

    Added some call backs and made eject optional

--- local/lib/python/encode_od/cli.py
+++ local/lib/python/encode_od/cli.py
@@ -1,6 +1,13 @@
 from subprocess import call, check_output, PIPE, Popen
-import os, sys, json, smtplib, datetime, shlex
+import os
+import sys
+import json
+import smtplib
+import datetime
+import shlex
 import click
+
+
 if float(sys.version[:3]) < 3.3:
     from pipes import quote
 else:
@@ -13,25 +20,28 @@ base_path = os.path.dirname(os.path.abspath(__file__))
 # assignment in main()
 global_logging = '/tmp'
 
+
 def eject(drive):
     if type(drive) is int:
         drive = 'sr%d' % drive
     shell(['eject', drive], shell=True)
 
+
 def get_dvd_name(source):
-    cmd = [' '.join([
-        "vobcopy", "-i", "/dev/sr%d" % source, "-I", "2>&1", ">", "/dev/stdout",
-        "|", "grep","DVD-name","|",
-        "sed","-e","'s/.*DVD-name: //'",
-        ])]
+    cmd = ' '.join([
+        "vobcopy -i /dev/sr%d -I 2>&1 > /dev/stdout" % source,
+        "| grep DVD-name | sed -e 's/.*DVD-name: //'",
+        ])
+
     r = Popen(cmd, shell=True, stdout=PIPE)
     (output, err) = r.communicate()
     result = output.decode('utf-8').strip()
     return result.replace('_', ' ').title()
 
-def ffmpeg(source,title):
+
+def ffmpeg(source, title):
     opts = {
-        'src': quote(os.path.join(source,'%s.mkv' % title)),
+        'src': quote(os.path.join(source, '%s.mkv' % title)),
         'dest': quote(os.path.join(source, '%s.m4v' % title)),
         'srt': quote(os.path.join(source, '%s.srt' % title)),
         }
@@ -42,14 +52,15 @@ def ffmpeg(source,title):
         opts['srt'] = ''
 
     result = shell([' '.join([
-            'ffmpeg','-i', opts['src'], '-i', opts['srt'],
+            'ffmpeg', '-i', opts['src'], '-i', opts['srt'],
             '-map', '0:v', '-map', '0:a',
             '-c:v', 'copy', '-vcodec', 'libx264',
             '-c:a', 'copy', '-acodec', 'aac', opts['dest']])
         ], shell=True)
-    return result
+    log(result)
 
-def handbrake(source,output):
+
+def handbrake(source, output):
     opts = {
         'src': quote(source),
         'dest': quote(output),
@@ -59,9 +70,10 @@ def handbrake(source,output):
         shell=True)
     return result
 
+
 def log(message, email_temp=False):
-    logfile = os.path.join(global_logging,'rip.log')
-    if type(message) not in [str,unicode]:
+    logfile = os.path.join(global_logging, 'rip.log')
+    if type(message) not in [str, unicode]:
         message = str(message)
     if os.path.isdir(global_logging):
         log_file = open(logfile, 'a')
@@ -73,7 +85,7 @@ def log(message, email_temp=False):
         send_email(message=message, **email_temp)
 
 
-def makemkv(source,output,title):
+def makemkv(source, output, title):
     log('Running makemkv')
     cmd = 'makemkvcon mkv --decrypt --cache=16 -r disc:%d all %s'
     shell([cmd % (source, output)], shell=True)
@@ -94,37 +106,47 @@ def makemkv(source,output,title):
     else:
         log(cmd % (source, output))
 
+
 @click.command()
 @click.option('--source', '-s', default=0,
-    help='Device Source Integer, ie /dev/sr0 [default: 0]')
+              help='Device Source Integer, ie /dev/sr0 [default: 0]')
 @click.option('--output', '-o', default=os.path.join(base_path, 'output'),
-    help='Absolute path of Output Directory [default: ./output]')
+              help='Absolute path of Output Directory [default: ./output]')
 @click.option('--force', is_flag=True, help='Force overwrite')
 @click.option('--no-logging', is_flag=True, help='Disable logging')
+@click.option('--eject-disc', is_flag=True, help='Eject\'s on completion')
 @click.option('--notify', '-n', default=False, multiple=True,
-    help='Email address to notify')
+              help='Email address to notify')
 @click.option('--email-sender', '-f', default='encode@optical.disc',
-    help='Email sender address')
+              help='Email sender address')
 @click.option('--email-host', '-h', default='localhost', help='Email provider')
 @click.option('--email-port', '-l', default=25, help='Email port')
 @click.option('--starttls', '-t', is_flag=True, help='Email starttls')
 @click.option('--email-user', '-u', default=False, help='Email user')
 @click.option('--email-pass', '-p', default=False, help='Email password')
-def main(source, output,force,no_logging,notify,email_sender,email_host,email_port,starttls,email_user,email_pass):
+@click.option('--event-start', default=False,
+              help='Shell execute this command on Start Event')
+@click.option('--event-disc-done', default=False,
+              help='Shell execute this command on Done with Disc Event')
+@click.option('--event-done', default=False,
+              help='Shell execute this command on All Done Event')
+def main(source, output, force, no_logging, eject_disc, notify, email_sender,
+         email_host, email_port, starttls, email_user, email_pass, event_start,
+         event_disc_done, event_done):
     # get the movie title
     title = get_dvd_name(source)
     email_temp = False
     if notify:
         email_temp = {
-        'sender': email_sender,
-        'receivers': notify,
-        'subject': 'Encoding %s' %title,
-        'host': email_host,
-        'port': email_port,
-        'starttls': starttls,
-        'user': email_user,
-        'password': email_pass,
-        }
+            'sender': email_sender,
+            'receivers': notify,
+            'subject': 'Encoding %s' % title,
+            'host': email_host,
+            'port': email_port,
+            'starttls': starttls,
+            'user': email_user,
+            'password': email_pass,
+            }
     log('Encoding started.', email_temp)
 
     # make necessary directories
@@ -134,6 +156,8 @@ def main(source, output,force,no_logging,notify,email_sender,email_host,email_po
     os.makedirs(output, exist_ok=True)
     # If this has already been done then kill this attempt unless forced
     if not os.path.isdir(groupP) or force:
+        if event_start:
+            shell(event_start, shell=True, standalone=True)
         os.makedirs(groupP, exist_ok=True)
 
         # rip to mkv
@@ -144,10 +168,19 @@ def main(source, output,force,no_logging,notify,email_sender,email_host,email_po
             log('MakeMKV encode Failure!:\n%s' % str(Err), email_temp)
             try:
                 log('Attempting to encode with HandBrake', email_temp)
-                handbrake('/dev/sr%d' % source, os.path.join(groupP, '%s.mkv' % title))
+                handbrake('/dev/sr%d' % source, os.path.join(groupP, '%s.mkv' %
+                                                             title))
             except Exception as Err:
-                log('HandBrake MKV encode Failure!:\n%s' % str(Err), email_temp)
+                log(
+                    'HandBrake MKV encode Failure!:\n%s' % str(Err),
+                    email_temp
+                    )
                 exit()
+        if event_disc_done:
+            shell(event_disc_done, shell=True, standalone=True)
+        if eject_disc:
+            eject(source)
+
         # Convert and insert subtitles
         log('Convert and insert subtitles')
         try:
@@ -165,21 +198,25 @@ def main(source, output,force,no_logging,notify,email_sender,email_host,email_po
 
         shell(['touch %s' % os.path.join(groupP, 'complete.done')])
         log('Encoding finished.', email_temp)
-        eject(source)
+        if event_done:
+            shell(event_done, shell=True, standalone=True)
     else:
-        log('This od has been encoded already. If you woul like to do it again plese use --force option.', email_temp)
+        log(' '.join([
+            'This od has been encoded already. If you woul like to do it',
+            'again plese use --force option.'
+            ]), email_temp)
 
 
 def send_email(**kwargs):
-    sender = kwargs.get('sender',False)
-    receivers = kwargs.get('receivers',False)
-    subject = kwargs.get('subject',False)
-    message = kwargs.get('message',False)
+    sender = kwargs.get('sender', False)
+    receivers = kwargs.get('receivers', False)
+    subject = kwargs.get('subject', False)
+    message = kwargs.get('message', False)
     host = kwargs.get('host', 'localhost')
     port = kwargs.get('port', 25)
     starttls = kwargs.get('starttls', False)
-    user = kwargs.get('user',False)
-    password = kwargs.get('password',False)
+    user = kwargs.get('user', False)
+    password = kwargs.get('password', False)
     if type(receivers) is str:
         recievers = [recievers]
     body = '\r\n'.join([
@@ -191,19 +228,20 @@ def send_email(**kwargs):
         ])
 
     try:
-       smtpObj = smtplib.SMTP(host, port)
-       smtpObj.ehlo()
-       if starttls:
-           smtpObj.starttls()
-       smtpObj.ehlo()
-       if user and password:
-           smtpObj.login(user,password)
-       smtpObj.sendmail(sender, receivers, body)
-       smtpObj.quit()
-       print("Successfully sent email")
+        smtpObj = smtplib.SMTP(host, port)
+        smtpObj.ehlo()
+        if starttls:
+            smtpObj.starttls()
+        smtpObj.ehlo()
+        if user and password:
+            smtpObj.login(user, password)
+        smtpObj.sendmail(sender, receivers, body)
+        smtpObj.quit()
+        print("Successfully sent email")
     except Exception as err:
-       print("Error: unable to send email")
-       print(err)
+        print("Error: unable to send email")
+        print(err)
+
 
 def convert_subtitles(output, title):
     sfiles = {
@@ -212,34 +250,34 @@ def convert_subtitles(output, title):
         'idx': quote(os.path.join(output, '%s.idx' % title)),
         'srt': quote(os.path.join(output, '%s.srt' % title)),
         'sub': quote(os.path.join(output, '%s' % title)),
-    }
+        }
     # get and index of mkv tracks
     r = shell(['mkvmerge -i -F json %(mkv)s' % sfiles], shell=True)
     subs = []
-    for track in json.loads(r).get('tracks',[]):
+    for track in json.loads(r).get('tracks', []):
         # ident english subtitle files and extract
         if track.get('type', '') in 'subtitles':
             subs.append(track.get('id'))
-            if track.get('properties',{}).get('language','') in 'eng':
-                sfiles['id'] = track.get('id',False)
+            if track.get('properties', {}).get('language', '') in 'eng':
+                sfiles['id'] = track.get('id', False)
 
     if sfiles['id']:
         print('Extracting subtitles... be patient this is not fast')
         shell([
             'mkvextract tracks %(mkv)s %(id)d:%(idx)s' % sfiles,
-        ],shell=True)
+        ], shell=True)
         print('Converting optical subtitles to text (OCR)...')
-        shell(['vobsub2srt %(sub)s' % sfiles,], shell=True)
+        shell(['vobsub2srt %(sub)s' % sfiles], shell=True)
         print('Subtitles converted, begining merge...')
-        shell([
-            "mkvmerge -o %(mkvCopy)s --subtitle-tracks %(id)d %(mkv)s %(srt)s" % sfiles,
-            ], shell=True)
+        c = "mkvmerge -o %(mkvCopy)s --subtitle-tracks %(id)d %(mkv)s %(srt)s"
+        shell([c % sfiles], shell=True)
         print('Cleaning up ...')
         os.remove(os.path.join(output, '%s.idx' % title))
         os.remove(os.path.join(output, '%s.sub' % title))
         os.remove(os.path.join(output, '%s.srt' % title))
         os.remove(os.path.join(output, '%s.mkv' % title))
-        os.rename(os.path.join(output, '%s-working.mkv' % title), os.path.join(output, '%s.mkv' % title))
+        os.rename(os.path.join(output, '%s-working.mkv' % title),
+                  os.path.join(output, '%s.mkv' % title))
         print('done.')
     else:
         print('Well damn it!')
@@ -247,9 +285,9 @@ def convert_subtitles(output, title):
 
 def shell(cmd, **kwargs):
     if type(cmd) == str:
-        cmd = cmd.split(' ')
+        cmd = shelex.split(cmd)
 
-    if kwargs.get('stream',False):
+    if kwargs.get('stream', False):
         call(cmd)
     elif kwargs.get('shell', False):
         r = Popen(cmd, stdout=PIPE, shell=True)
@@ -261,6 +299,8 @@ def shell(cmd, **kwargs):
                 log(err.decode('utf-8'))
         else:
             return output.decode('utf-8')
+    elif kwargs.get('standalone', False):
+        return Popen(cmd).pid
     else:
         return check_output(cmd)
 
